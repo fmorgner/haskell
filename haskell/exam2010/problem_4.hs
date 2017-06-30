4a5bdc67fce850970d4454f05fe6805804f8ce8c
Felix Morgner <felix.morgner@gmail.com>
2017-06-30 13:46:50 +0200

    exam2010: rephrase and expand problem statements

--- haskell/exam2010/problem_4.hs
+++ haskell/exam2010/problem_4.hs
@@ -1,29 +1,45 @@
 module ExamPreparation.Problem4 where
 
+{-
+Given the following ADTs representing grammar elements of an imaginary language,
+Solve the sub-problems below.
+-}
 data Type  = BoolTy | IntTy deriving (Eq, Show)
 data Value = BoolVal Bool | IntVal Int
 data Opr   = Plus | Equal | And deriving (Eq)
-data Expr = Const Value
-          | Var Ident
-          | Dyadic Opr Expr Expr
+data Expr  = Const Value
+           | Var Ident
+           | Dyadic Opr Expr Expr
 type Ident = String
-type Env = Ident -> Type
 
-{- Sub-problem 1:
-  Provide the *ABSTRACT* syntax for:
-
+{-
+Sub-problem 1: Provide the *ABSTRACT* syntax for:
   (a) 1 + true
   (b) x + 3
 -}
-
 a = Dyadic Plus (Const (IntVal 1)) (Const (BoolVal True))
 b = Dyadic Plus (Var "x") (Const (IntVal 3))
 
-{- Sub-problem 2:
-  (a) Provide all valid typings for the following expression:
+{-
+Sub-problem 2a: Given the environment type below, Provide all valid typings for
+the expression: ((x + 1) = y) AND (p = q)
+-}
+type Env   = Ident -> Type
 
-      ((x + 1) = y) AND (p = q)
+env1 :: Env
+env1 "x" = IntTy
+env1 "y" = IntTy
+env1 "p" = IntTy
+env1 "q" = IntTy
 
+env2 :: Env
+env2 "x" = IntTy
+env2 "y" = IntTy
+env2 "p" = BoolTy
+env2 "q" = BoolTy
+
+{-
+Sub-problem 2b & 2c:
   (b) Given a function check, that takes an expression and an environment, and
       returns 'Just Type' if the expression is well-typed or 'Nothing' if it
       isn't, provide the type of 'check'.
@@ -31,16 +47,6 @@ b = Dyadic Plus (Var "x") (Const (IntVal 3))
   (c) Implement check. Assume that every variable occuring in the expression
       also exists in the environment.
 -}
-
-spe1 "x" = IntTy
-spe1 "y" = IntTy
-spe1 "p" = IntTy
-spe1 "q" = IntTy
-
-spe2 "x" = IntTy
-spe2 "y" = IntTy
-spe2 "p" = BoolTy
-spe2 "q" = BoolTy
 
 check :: Expr -> Env -> Maybe Type
 check (Const (BoolVal _)) _ = Just BoolTy
