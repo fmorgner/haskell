12dd9d359ec37a914cda0dfa0b5086768312c7ac
Felix Morgner <felix.morgner@gmail.com>
2017-06-30 13:29:17 +0200

    exam2010: add problem statements

--- haskell/exam2010/problem_3.hs
+++ haskell/exam2010/problem_3.hs
@@ -1,22 +1,35 @@
 module ExamPreparation.Problem3 where
 
+{-
+Imagine a function 'repfor' that takes two parameters 'n' and 'f', where 'n' is
+the number of times it will apply 'f'. For example:
+  thrice f = repfor 3 f
+
+(a) Provide the most generic type for 'repfor'
+(2) Implement 'repfor'
+-}
 repfor :: (Ord a, Num a) => a -> (b -> b) -> b -> b
 repfor n f
   | n > 0 = f . repfor (n - 1) f
   | otherwise = id
 
--- repfor' :: Int -> (b -> b) -> b -> b
+{-
+Alternative implementation with DIFFERENT(!!!) type
+-}
+repfor' :: Int -> (b -> b) -> b -> b
 repfor' n f x = foldr id x (replicate n f)
 
 {-
+Demonstrate that 'repfor 3 = thrice':
 
-repfor 3 f
-
--> (f . repfor 2 f)
--> (f . f . repfor 1 f)
--> (f . f . f . repfor 0 f)
--> (f . f . f . id)
-
+repfor 3 f =>
+  -> (f . repfor 2 f)
+  -> (f . f . repfor 1 f)
+  -> (f . f . f . repfor 0 f)
+  -> (f . f . f . id)
 -}
 
+{-
+Implement the summation of all integral numbers in the range 0..n using repfor.
+-}
 sumOn n = snd $ repfor n (\(i, s) -> (i + 1, i + s)) (0, 0)
